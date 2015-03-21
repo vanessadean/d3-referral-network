5bc22d126128de12fe2a245197df38ef5bfd27ad
vanessadean <vanessa.dean@gmail.com>
2015-03-21 10:02:31 -0400

    data for each doctor

---
+++ parser/doctor_parse.rb
@@ -0,0 +1,70 @@
+require 'csv'
+require 'json'
+
+def parse_for(doctor)
+  data = {}
+  data["nodes"] = []
+  data["links"] = []
+
+  targets = []
+
+  CSV.foreach("../data/DataForVanessa.csv") do |row|
+    if row[0] == doctor && row[0] != row[3]
+      node1 = data["nodes"].detect { |node| node["name"] == row[0] }
+      node2 = data["nodes"].detect { |node| node["name"] == row[3] }
+
+      if node1
+        node1["total_referrals"] += 1
+      else
+        data["nodes"] << {"name" => row[0], "practice" => row[1], "specialty" => row[2], "total_referrals" => 1}
+      end
+
+      if node2
+        node2["total_referrals"] += 1
+      else
+        data["nodes"] << {"name" => row[3], "practice" => row[4], "specialty" => row[5], "total_referrals" => 1}
+      end
+
+      unless data["links"].any? { |link| link == { "source" => row[0], "target" => row[3] } }
+        data["links"] << { "source" => row[0], "target" => row[3] }
+        targets << row[3]
+      end
+    end
+  end
+
+  CSV.foreach("../data/DataForVanessa.csv") do |row|
+    targets.each do |target|
+      if row[0] == target && row[0] != row[3]
+        node1 = data["nodes"].detect { |node| node["name"] == row[0] }
+        node2 = data["nodes"].detect { |node| node["name"] == row[3] }
+
+        if node1
+          node1["total_referrals"] += 1
+        else
+          data["nodes"] << {"name" => row[0], "practice" => row[1], "specialty" => row[2], "total_referrals" => 1}
+        end
+
+        if node2
+          node2["total_referrals"] += 1
+        else
+          data["nodes"] << {"name" => row[3], "practice" => row[4], "specialty" => row[5], "total_referrals" => 1}
+        end
+
+        unless data["links"].any? { |link| link == { "source" => row[0], "target" => row[3] } }
+          data["links"] << { "source" => row[0], "target" => row[3] }
+        end
+      end
+    end
+  end
+
+  File.open("../data/#{doctor.split(" ").last.downcase}.json", 'w') do |file|
+    file.puts JSON.pretty_generate(JSON.parse(data.to_json))
+  end
+end
+
+parse_for("Howard Baruch")
+parse_for("Christine Corradino")
+parse_for("Mohammad Dorri")
+parse_for("Danielle Groves")
+parse_for("Danielle Zelnik")
+parse_for("Iris Drey")
